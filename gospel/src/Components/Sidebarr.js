1ebf6cfcb01d03884d49c100deb09716baa533da
frank_kiruma <frankkiruma05@gmail.com>
2023-11-17 15:18:07 +0300

    added sidebar and navbar

---
+++ gospel/src/Components/Sidebarr.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div
+      style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}
+    >
+      <CDBSidebar textColor="#fff" backgroundColor="#333">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <a
+            href="/"
+            className="text-decoration-none"
+            style={{ color: 'inherit' }}
+          >
+            Everlasting Gospel
+          </a>
+        </CDBSidebarHeader>
+
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <NavLink exact to="/" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/Documentary" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="table">Documentary</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/Quotes" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="chart-line">Quotes</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/MorningEvolution" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Morning Evolution</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/profile" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/analytics" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="chart-line">
+                Analytics
+              </CDBSidebarMenuItem>
+            </NavLink>
+
+            <NavLink
+              exact
+              to="/hero404"
+              target="_blank"
+              activeClassName="activeClicked"
+            >
+              <CDBSidebarMenuItem icon="exclamation-circle">
+                404 page
+              </CDBSidebarMenuItem>
+            </NavLink>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+
+        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            // style={{
+            //   padding: '10px 5px',
+            // }}
+          >
+            <hr/>
+           @civeDeverlopers
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+    </div>
+  );
+};
+
+export default Sidebar;
